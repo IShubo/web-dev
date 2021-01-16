b03aecb0ba296b6ac81df95def8e301c6ab1f105
ABM Imran Haydar <abmhaydar@gmail.com>
2021-01-16 17:13:07 -0500

    Completed Section 15 - JavaScript Strings and More

---
+++ s15-js-strings-and-more/first-strings-practice.js
@@ -0,0 +1,6 @@
+// Please define the following string variables (you can use either let or const):
+// bestColor should be set to the string "purple"
+// quote should be set to the string "You had me at "hello"" - be careful about quotation marks, we want double quotes inside the string
+
+let bestColor = "purple";
+let quote = 'You had me at "hello"';
