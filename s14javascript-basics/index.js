26bfbf171d1f0a5a0fd08f482ff375bf4252dd2d
ABM Imran Haydar <abmhaydar@gmail.com>
2021-01-16 14:44:05 -0500

    Sec 14 JavaScript Basics Finished

---
+++ s14javascript-basics/index.js
@@ -0,0 +1,16 @@
+// Exercise 15: First Variables Ex
+// Define a variable called myLuckyNumber and set it equal to 37
+// Define a variable called octopucLimbs and set it equal to 8
+
+let myLuckyNumber = 37;
+let octopusLimbs = 8;
+
+
+// Exercise 16: First Constants Ex
+// Define a constant called boilingPointC and set it to 100
+// Define a second constant called boilingPointF and set it to 212
+
+const boilingPointC = 100;
+const boilingPointF = 212;
+
+
